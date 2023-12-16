475bdff3ab66f8d008c13b020463def80189f0f8
Kamla Kasichainula <kamlakasichainula@macbook-pro.lan>
2023-12-15 20:28:31 -0500

    giving up on day3 pt 2 for now

--- day3/day3_pt2.py
+++ day3/day3_pt2.py
@@ -8,80 +8,67 @@ len_schematic_row = len(schematic[0])
 len_schematic_col = len(schematic)
 
 def get_adjacent_num_locations(x, y):
-    adjacent_locations = []
+    adjacent_part_nums = []
     if x > 0:
         # top left
         if y > 0:
             test_value = schematic[x-1][y-1]
             if test_value.isdigit():
-                adjacent_locations.append((x-1, y-1))
+                adjacent_part_num = get_adjacent_num_sum(x-1, y-1)
+                if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                    adjacent_part_nums.append(adjacent_part_num)
         # top
         test_value = schematic[x-1][y]
         if test_value.isdigit():
-            adjacent_locations.append((x-1, y))
+            adjacent_part_num = get_adjacent_num_sum(x-1, y)
+            if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                adjacent_part_nums.append(adjacent_part_num)
 
         # top_right
         if y < len_schematic_row-1:
             test_value = schematic[x-1][y+1]
             if test_value.isdigit():
-                adjacent_locations.append((x-1, y+1))
+                adjacent_part_num = get_adjacent_num_sum(x-1, y+1)
+                if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                    adjacent_part_nums.append(adjacent_part_num)
     # left
     if y > 0:
         test_value = schematic[x][y-1]
         if test_value.isdigit():
-            adjacent_locations.append((x, y-1))
+            adjacent_part_num = get_adjacent_num_sum(x, y-1)
+            if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                adjacent_part_nums.append(adjacent_part_num)
 
     # right
     if y < len_schematic_row-1:
         test_value = schematic[x][y+1]
         if test_value.isdigit():
-            adjacent_locations.append((x, y+1))
+            adjacent_part_num = get_adjacent_num_sum(x, y+1)
+            if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                adjacent_part_nums.append(adjacent_part_num)
     # bottom_left
     if x < len_schematic_col - 1:
         if y > 0:
             test_value = schematic[x+1][y-1]
             if test_value.isdigit():
-                adjacent_locations.append((x+1, y-1))
+                adjacent_part_num = get_adjacent_num_sum(x+1, y-1)
+                if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                    adjacent_part_nums.append(adjacent_part_num)
         # bottom
         test_value = schematic[x+1][y]
         if test_value.isdigit():
-            adjacent_locations.append((x+1, y))
+            adjacent_part_num = get_adjacent_num_sum(x+1, y)
+            if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                adjacent_part_nums.append(adjacent_part_num)
         # bottom_right
         if y < len_schematic_row-1:
             test_value = schematic[x+1][y+1]
             if test_value.isdigit():
-                adjacent_locations.append((x+1, y+1))
+                adjacent_part_num = get_adjacent_num_sum(x+1, y+1)
+                if len(adjacent_part_nums) == 0 or adjacent_part_num != adjacent_part_nums[-1]:
+                    adjacent_part_nums.append(adjacent_part_num)
 
-    return adjacent_locations
-
-
-
-# def get_part_num_locations(schematic):
-#     sum_part_nums = 0
-#     for xidx, x in enumerate(schematic):
-#         currently_in_number = False
-#         current_num_has_symbol = False
-#         current_num_in = ''
-#         for yidx, y in enumerate(x):
-#             if y.isdigit():
-#                 currently_in_number = True
-#                 current_num_in += y
-#                 if not current_num_has_symbol:
-#                     if has_symbol_adjacent(xidx, yidx):
-#                         current_num_has_symbol = True
-#                 if yidx == len(x) - 1:
-#                     if current_num_has_symbol:
-#                         sum_part_nums += int(current_num_in)
-#             else:
-#                 if currently_in_number:
-#                     if current_num_has_symbol:
-#                         sum_part_nums += int(current_num_in)
-#                 currently_in_number = False
-#                 current_num_in = ''
-#                 current_num_has_symbol = False
-#                 continue
-
-#     return sum_part_nums
+    return adjacent_part_nums
 
 
 def get_adjacent_num_sum(xidx, yidx):
@@ -89,13 +76,22 @@ def get_adjacent_num_sum(xidx, yidx):
     row = schematic[xidx]
     current_yidx = yidx
     current_part_num =''
-    while row[current_yidx].isdigit():
-        current_part_num += schematic[xidx][current_yidx]
-        current_yidx+=1
+    print(f'whats the current yidx, xidx and length {yidx}, {xidx}, {len(schematic[xidx])}')
+    if current_yidx <140:
+        while row[current_yidx].isdigit():
+            current_part_num += schematic[xidx][current_yidx]
+            if current_yidx == 139:
+                break
+            else:
+                current_yidx+=1
     current_left_yidx = yidx-1
-    while row[current_left_yidx].isdigit():
-        current_part_num = schematic[xidx][current_left_yidx] + current_part_num
-        current_left_yidx-=1
+    if current_left_yidx >= 0:
+        while row[current_left_yidx].isdigit():
+            current_part_num = schematic[xidx][current_left_yidx] + current_part_num
+            current_left_yidx-=1
+            if current_left_yidx < 0:
+                break
+    print(f'current part num is {current_part_num}')
     
     return int(current_part_num)
 
@@ -106,14 +102,15 @@ def get_adjacent_num_sum(xidx, yidx):
 def get_gear_part_sum(schematic):
     sum_gear_part_nums = 0
     for xidx, x in enumerate(schematic):
-        currently_in_number = False
-        current_num_has_symbol = False
-        current_num_in = ''
         for yidx, y in enumerate(x):
             if y=='*':
-                adjacent_num_locations = get_adjacent_num_locations(xidx, yidx)
-                if len(adjacent_num_locations) == 2:
-                    sum_gear_part_nums += get_adjacent_num_sum(adjacent_num_locations[0][0], adjacent_num_locations[0][1]) + get_adjacent_num_sum(adjacent_num_locations[1][0], adjacent_num_locations[1][1])
+                adjacent_part_nums = get_adjacent_num_locations(xidx, yidx)
+                print(f'the adjacent_part_nums_are {adjacent_part_nums}')
+                if len(adjacent_part_nums) != 2:
+                    print('stop!')
+                else:
+                    sum_gear_part_nums = sum_gear_part_nums + (adjacent_part_nums[0] * adjacent_part_nums[1])
+                    print(sum_gear_part_nums)
     
     return sum_gear_part_nums
 
